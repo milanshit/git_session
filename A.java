43b9e248940087e2e3fc431f66d4d8d4e306c8be
milanshit <milanshit941@gmail.com>
2023-03-05 23:34:57 +0530

    First commit

---
+++ A.java
@@ -0,0 +1,10 @@
+class A
+{
+  public static void main(String args[])
+  {
+    System.out.println("Parameter Recived");
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+    System.out.println(args[2]);
+   }
+}
