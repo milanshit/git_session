9a6099f497074344f0b78f85d9937e8cf20533ce
milanshit <milanshit941@gmail.com>
2023-03-13 20:59:09 +0530

    second commit

---
+++ half_pyramid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class half_pyramid {
+    public static void main(String[] args){
+        Scanner ob=new Scanner(System.in);
+        System.out.print("Enter the length and width:");
+        int n=ob.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        }
+    }
+}
